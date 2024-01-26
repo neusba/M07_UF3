499d99bfbdaa8fa30b7d7a5e7c43b9ccdacc881a
Neus Bravo Arias <cf22neus.bravo@iesjoandaustria.org>
2024-01-26 16:14:33 +0100

    Ej 3 terminado

---
+++ REPAS/exercici3.py
@@ -0,0 +1,8 @@
+# Demanar a l’usuari que introdueixi 2 valors i mostrar, per pantalla, quin és el més gran.
+valor1 = int(input('Introduce un valor'))
+valor2 = int(input('Introduce otro valor'))
+
+if (valor1 > valor2):
+    print('El valor más grande es:', valor1)
+else: 
+    print('El valor más grande es:', valor2)
