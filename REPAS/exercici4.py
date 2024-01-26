2ead54f759fe5b45e77587713c890aba51e9daef
Neus Bravo Arias <cf22neus.bravo@iesjoandaustria.org>
2024-01-26 16:16:55 +0100

    Ej 4 terminado

---
+++ REPAS/exercici4.py
@@ -0,0 +1,5 @@
+# Demanar a l’usuari dos valors. Una vegada s’executa el programa, aquest ha de mostrar el resultat per pantalla.
+valor1 = int(input('Introduce un valor: '))
+valor2 = int(input('Introduce otro valor: '))
+
+print('El resultado es:', (valor1 + valor2))
