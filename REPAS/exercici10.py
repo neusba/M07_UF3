64738f2f1dec3fe905633d45612b2a787ac13c46
Neus Bravo Arias <cf22neus.bravo@iesjoandaustria.org>
2024-01-31 17:19:08 +0100

    Parte que faltaba de ej 10

--- REPAS/exercici10.py
+++ REPAS/exercici10.py
@@ -1,10 +1,14 @@
 # Crear un arxiu on caldrà endevinar el número escollit pel programa entre 1 i 100. Cada vegada que l’usuari hi posi un número, caldrà indicar si és més petit o més gran fins que encerti i el missatge haurà d’indicar que ha encertat. Indicar també el número d’intents.
 import random
 
+intentos = 0
 misterioso = random.randint(1, 100)
-numero = int(input('Introduce un número: '))
+numero = int(input('Introduce un número entre el 1 y el 100: '))
 
 while numero != misterioso:
-    numero = int(input('Introduce un número: '))
+    intentos += 1
+    print('El número a adivinar es más pequeño') if numero > misterioso else print('El número a adivinar es más grande')
+    numero = int(input('Introduce un número entre el 1 y el 100: '))
 
-print('Has acertado! El número era el', misterioso)
+print('Has encertado!')
+print('Intentos:', intentos, '\nEl número era', misterioso)
