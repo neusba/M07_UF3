d678b48d6d489d826efe865c3468332440e1b439
Neus Bravo Arias <cf22neus.bravo@iesjoandaustria.org>
2024-02-09 16:00:35 +0100

    Commit de clase: avance en funciones de crear la tabla y leer la bbdd

---
+++ PRACTICA2/main.py
@@ -0,0 +1,10 @@
+# En este archivo veremos la ejecución de todo el CRUD
+
+import psycopg2     # Adaptador de postgreSQL para Python
+import connection   # Conexión a la bbdd
+import create_table
+import create
+import read
+import update
+import delete
+
