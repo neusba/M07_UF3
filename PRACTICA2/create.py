b64cdabf066a2d4d3e77d7021ec5fe4ff79fa469
Neus Bravo Arias <cf22neus.bravo@iesjoandaustria.org>
2024-02-09 17:23:38 +0100

    Commit de clase: funciones terminadas quedaría organizar las llamadas del main y reestructurar codigo repetido :)

--- PRACTICA2/create.py
+++ PRACTICA2/create.py
@@ -1,33 +1,17 @@
-import psycopg2
+# Archivo en el que insertamos datos de prueba para comprobar que aparecen posteriormente en la base de datos seleccionada
 import connection
 
-try:
+def create():
 
-    sql = '''INSERT INTO pets(pet_id, pet_name, pet_surname, pet_age, pet_type, pet_color) VALUES (%s,%s,%s,%s,%s)'''
+    sql = '''INSERT INTO pets(pet_id, pet_name, pet_surname, pet_age, pet_type, pet_color) VALUES (%s,%s,%s,%s,%s,%s)'''
     
-    record_to_insert = [(1, 'Packt', 1950,
-                         'chennai', 'books'),
-                        (2, 'Springer', 1950,
-                         'chennai', 'books'),
-                        (3, 'Springer', 1950,
-                         'chennai', 'articles'),
-                        (4, 'Oxford', 1950,
-                         'chennai', 'all'),
-                        (5, 'MIT', 1950,
-                         'chennai', 'books')]
+    datos = [(1, 'Hamlet', 'Bravo', 3, 'Cat', 'Grey/White'),
+             (2, 'Peanut', 'Bravo', 2, 'Cat', 'Orange'),
+             (3, 'Jaggy', 'Bravo', 1, 'Cat', 'White'),
+             (4, 'Coco', 'Channel', 3, 'Dog', 'White/Brown'),
+             (5, 'Rayo', 'Macuin', 4, 'Dog', 'Black/Brown')]
     
-    for i in record_to_insert:
+    print('Insertando datos...')
+    for i in datos:
         connection.connection.execute(sql, i)
         connection.conn.commit()
-
-        count = connection.conn.rowcount
-        print(count, "Record inserted successfully into publisher table")
- 
-except (Exception, psycopg2.Error) as error:
-    print("Error al insertar en la tabla", error)
- 
-finally:
-    if connection:
-        connection.close()
-        print("GOODBYE")
-
--- PRACTICA2/create_table.py
+++ PRACTICA2/create_table.py
@@ -1,10 +1,10 @@
+# En este archivo crearemos la query para crear la tabla PETS y se la enviaremos a la base de datos
+
 import connection
-import psycopg2
 
-try:
+def createTable():
 
-    # Archivo para crear la tabla de la BBDD (6 campos)
-    sql = '''CREATE TABLE PETS(
+    sql = '''CREATE TABLE IF NOT EXISTS PETS(
                 pet_id SERIAL PRIMARY KEY,
                 pet_name VARCHAR(255) NOT NULL,
                 pet_surname VARCHAR(255) NOT NULL,
@@ -20,9 +20,3 @@ try:
     connection.connection.execute(sql)
     # Método para hacer efectivos los cambios en la BBDD
     connection.conn.commit()
-
-except (Exception, psycopg2.Error) as error:
-    print("Error:", error)
-finally:
-    print('GOODBYE')
-    connection.conn.close()
